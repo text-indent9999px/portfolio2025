791745b13d4b60815ec472944cbe01375e3a5448
text-indent9999px <namyoung1013@gmail.com>
2025-12-09 02:58:05 +0900

    chore: 문서 수정일 최신화

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -74,7 +74,7 @@ export default function Home() {
                 break-keep
               `}
           >
-            last updated: 2025.12.04
+            last updated: 2025.12.09
           </li>
         </ul>
       </div>
