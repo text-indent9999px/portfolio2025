21f16912ff0ac2a4c0ab1b37c7e12496dc946c03
text-indent9999px <namyoung1013@gmail.com>
2025-12-12 20:09:54 +0900

    refactor: ResumeDownloadButton UI 및 구조 개선
    - UI 형태 변경
    - 컴포넌트 구조 리팩토링

--- src/components/ui/Button/variants/ResumeDownloadButton.tsx
+++ src/components/ui/Button/variants/ResumeDownloadButton.tsx
@@ -2,7 +2,6 @@
 
 import { faCircleArrowDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useMediaQuery } from '../../../../hooks';
 import Button from '../Button';
 import type { CustomButtonProps } from '../Button.types';
 
@@ -21,29 +20,16 @@ export type ResumeDownloadButtonProps = Omit<CustomButtonProps, 'onClick'> & {
 };
 
 export default function ResumeDownloadButton({
-  variant = 'filled',
-  color = 'primary',
   size = 'md',
   rounded = 'none',
   style,
+  className,
   ...rest
 }: ResumeDownloadButtonProps) {
-  const isXlOrAbove = useMediaQuery('--breakpoint-xl', 'min');
   const hasLargePadding = size !== 'xs' && size !== 'sm';
-
-  const hoverButtonClassName = [
-    'absolute! top-0 left-0 w-full h-full origin-center',
-    '[clip-path:polygon(0_50%,100%_50%,100%_50%,0_50%)]',
-    'group-hover:[clip-path:polygon(0_0,100%_0,100%_100%,0%_100%)]',
-    'focus-visible:[clip-path:polygon(0_0,100%_0,100%_100%,0%_100%)]',
-    'focus-visible:outline-none focus-visible:ring-4 focus-visible:ring-accent-300! focus-visible:ring-offset-0 focus-visible:ring-inset',
-    'transition-all duration-[0.4s] ease-[cubic-bezier(0.1,0.5,0.5,1)]',
-    'font-bold font-eng-point uppercase',
-    'bg-primary-300! text-primary-900! border-transparent!',
-  ].join(' ');
-
-  const defaultButtonClassName = `font-semibold uppercase font-kor-point 
-    focus-visible:ring-4 focus-visible:ring-accent-300!`;
+  const buttonClassName = `font-semibold uppercase font-kor-point 
+    focus-visible:ring-4 focus-visible:ring-accent-300! 
+    `;
 
   return (
     <span className="relative inline-flex group">
@@ -53,32 +39,15 @@ export default function ResumeDownloadButton({
         size={size}
         rounded={rounded}
         icon={<FontAwesomeIcon icon={faCircleArrowDown} />}
-        className={`${defaultButtonClassName} ${
+        className={`${buttonClassName} ${
           hasLargePadding ? 'gap-2 px-8! py-3!' : ''
-        }`}
+        } ${className}`}
         style={style}
-        tabIndex={isXlOrAbove ? -1 : undefined}
-        aria-hidden={isXlOrAbove ? 'true' : undefined}
+        onClick={downloadResume}
         {...rest}
-        noHoverActive={isXlOrAbove ? true : false}
-        onClick={isXlOrAbove ? undefined : downloadResume}
       >
         이력서 다운로드
       </Button>
-      {isXlOrAbove && (
-        <Button
-          onClick={downloadResume}
-          variant={variant}
-          color={color}
-          size={size}
-          rounded={rounded}
-          className={hoverButtonClassName}
-          {...rest}
-          noHoverActive={true}
-        >
-          <span className={`text-[1.6em]`}>download</span>
-        </Button>
-      )}
     </span>
   );
 }
